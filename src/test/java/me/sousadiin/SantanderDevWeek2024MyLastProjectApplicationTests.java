8ecbe6b964f7027ea95837cfeab2e37dd0bea383
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2024-07-29 22:34:02 -0300

    Initial commit

---
+++ src/test/java/me/sousadiin/SantanderDevWeek2024MyLastProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sousadiin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2024MyLastProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
